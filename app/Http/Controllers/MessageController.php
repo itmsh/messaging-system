c4192acf17de8cbdc547374549b6a6706345e60a
masoud <masoud.sharifi@tourware.net>
2023-07-19 13:19:55 +0330

    feat: new message list and post message is added

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the message.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index(User $user, Request $request)
+    {
+        $authUser = $request->user();
+        return view('message', [
+            'selectedUser' => $user,
+            'users'        => User::get(),
+            'messages'      => Message::where(function ($q) use ($authUser, $user) {
+                $q->where('from_user_id', $authUser->id)
+                  ->where('to_user_id', $user->id);
+            })->orWhere(function ($q) use ($authUser, $user) {
+                $q->where('from_user_id', $user->id)
+                  ->where('to_user_id', $authUser->id);
+            })->get()
+        ]);
+    }
+
+    /**
+     * Store a newly created message in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(User $user, Request $request)
+    {
+        $message               = new Message();
+        $message->message      = $request->get('message');
+        $message->from_user_id = $request->user()->id;
+        $message->to_user_id   = $user->id;
+        $message->save();
+
+        return redirect()->route('message', $user->id);
+    }
+
+    /**
+     * Update the specified message in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified message from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('{user}/messages', MessageController::class)
+         ->except(['update', 'create', 'show', 'edit'])
+         ->name('index','message');
 });
 
 require __DIR__.'/auth.php';
